b4394d39316230b031560608451fd6b82c42c669
curiosag <curiosa.globunznik@gmail.com>
2016-03-09 13:54:08 +0100

    statusPredicted added

--- src/org/cg/ads/aads/Ad.java
+++ src/org/cg/ads/aads/Ad.java
@@ -9,6 +9,7 @@ public class Ad {
 	private final static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
 	public final int id;
+	public final int status;
 	private int statusPredicted;
 	public final double prize;
 	public final double rooms;
@@ -19,9 +20,10 @@ public class Ad {
 	public final String description;
 	public final Date timestamp;
 
-	public Ad(int id, int statusPredicted, double prize, double rooms, double size, String url, String location, String phone,
+	public Ad(int id, int status, int statusPredicted, double prize, double rooms, double size, String url, String location, String phone,
 			String description, Date timestamp) {
 		this.id = id;
+		this.status = status;
 		this.setStatusPredicted(statusPredicted);
 		this.prize = prize;
 		this.rooms = rooms;
@@ -33,10 +35,11 @@ public class Ad {
 		this.timestamp = timestamp;
 	}
 
-	public Ad(String id, String status, String prize, String rooms, String size, String url, String location,
+	public Ad(String id, String status, String statusPredicted, String prize, String rooms, String size, String url, String location,
 			String phone, String description, String timestamp) {
 		this.id = Integer.parseInt(id);
-		this.setStatusPredicted(Integer.parseInt(status));
+		this.status = Integer.parseInt(status);
+		this.setStatusPredicted(Integer.parseInt(statusPredicted));
 		this.prize = Double.parseDouble(prize);
 		this.rooms = Double.parseDouble(rooms);
 		this.size = Double.parseDouble(size);
@@ -56,14 +59,13 @@ public class Ad {
 	}
 
 	private final static String valuesTemplate = " (%d, %d, %d, %.2f, %.2f, %.2f, '%s', '%s', '%s', '%s', '%s')";
-	private static final int STATUS_NOT_SET = 0;
 
 	@SuppressWarnings("boxing")
 	public String getInsertStatement(String storageTableId) {
 		return "insert into "
 				+ storageTableId
 				+ " (id, status, statusPredicted, prize, rooms, size, url, location, phone, description, timestamp) values "
-				+ String.format(valuesTemplate, id, STATUS_NOT_SET, getStatusPredicted(), prize, rooms, size, url, location, phone, description,
+				+ String.format(valuesTemplate, id, status, getStatusPredicted(), prize, rooms, size, url, location, phone, description,
 						DATE_FORMAT.format(timestamp));
 	}
 
